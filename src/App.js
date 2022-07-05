db9ecd4451b507e6637c42d0eaba0ef48ea6d7e4
Pranjal-111 <84219851+Pranjal-111@users.noreply.github.com>
2022-07-05 11:44:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './Pages/Home';
+import Bitcoin from './Pages/Bitcoin';
+import {BrowserRouter, Route} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import  Alert from './components/Alert';
 
 function App() {
+  const useStyle = makeStyles(()=>({
+    App:{
+      backgroundColor:"#14161a",
+      color:"#04b5e5",
+      minHeight:'100vh'
+
+
+    }
+  }))
+  const classes = useStyle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <BrowserRouter>
+    <div className={classes.App}>
+    <Header/>
+    <Route path='/' component={Home} exact ></Route>
+    <Route path='/coins/:id' component={Bitcoin} exact ></Route>
     </div>
+    <Alert/>
+    </BrowserRouter>
+ </>
   );
 }
 
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyCa74qMeMvztxACNISEihhTyYS3KnB4U",
+    authDomain: "crypto-hunter-b6452.firebaseapp.com",
+    projectId: "crypto-hunter-b6452",
+    storageBucket: "crypto-hunter-b6452.appspot.com",
+    messagingSenderId: "508486472047",
+    appId: "1:508486472047:web:de078549421ca3ff59d89a",
+    measurementId: "G-QFEZM8Y4Q9"
+  };
+
+
+  export default firebaseConfig;
